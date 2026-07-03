1bfa40448936cc8d809c368d52598f2e939d9524
Md Nehal <nehalmd2955@gmail.com>
2026-07-04 00:56:02 +0530

    Automated Sync: Solved Shortest Common Supersequence

--- shortest_common_supersequence/Solution.cpp
+++ shortest_common_supersequence/Solution.cpp
@@ -1,33 +1,8 @@
-0;i<=m;++i){
-            dp[0][i] = i;
-        }
-        for(int i=0;i<=n;++i){
-            dp[i][0] = i;
-        }
-        for(int i=1;i<=n;++i){
-            for(int j=1;j<=m;++j){
-                dp[i][j] = min(dp[i][j-1]+1,dp[i-1][j]+1);
-                if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = min(dp[i][j],dp[i-1][j-1]+1);
-                }
-            }
-        }
-        int i= n,j=m;
-        int val = dp[n][m];
-        string ans;
-        while(i !=0 || j !=0){
-            if(i>0 && dp[i-1][j]+1 == val){
-                ans.push_back(str1[i-1]);
-                val = dp[i-1][j];
-                i--;
-                continue;
-            }
-            if(j>0 && dp[i][j-1]+1 == val){
-                ans.push_back(str2[j-1]);
-                val = dp[i][j-1];
-                j--;
-                continue;
-            }
-            if(i>0 && j>0 && str1[i-1] == str2[j-1]  && dp[i-1][j-1] +1 == val){
-                ans.push_back(str1[i-1]);
-                val = dp[i
+                i--,j--;
+                continue;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
