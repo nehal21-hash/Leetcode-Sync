8e7bbc8cedb6c88b38f5d08d01fd7fa3ac751dd7
Md Nehal <nehalmd2955@gmail.com>
2026-07-09 13:55:11 +0530

    Automated Sync: Solved Path Existence Queries In A Graph I

--- path_existence_queries_in_a_graph_i/Solution.cpp
+++ path_existence_queries_in_a_graph_i/Solution.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    vector<int>parent,ranks;
-    int find_set(int u){
-        if(parent[u] == u)return u;
-
-        return parent[u] = find_set(parent[u]);
-    }
-    void union_set(int a,int b){
-        int u = find_set(a),v = find_set(b);
-        if(u == v){
-            return ;
-        }
-        if(ranks[u]>ranks[v]){
-            ranks[u]+=ranks[v];
-            parent[v] = u;
-        }else{
-            ranks[v]+=ranks[u];
-            parent[u] = v;
-        }
-        return ;
-    }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        parent.resize(n+1);
-        ranks.resize(n+1);
+        vector<int>v(n);
         int i=0,j=0;
-        for(int i=0;i<n;++i){
-            parent[i] = i;
-            ranks[i] = 1;
-        }
+        int cnt = 0;
         while(j<n){
+            if(i == j){
+                cnt++;
+            }
             if(abs(nums[i]-nums[j])>maxDiff){
                 i++;
                 continue;
             }
-            union_set(i,j);
+            v[j] = cnt;
             j++;
         }
         vector<bool>ans;
         for(auto &x:queries){
-            ans.push_back(find_set(x[0]) == find_set(x[1]));
+            ans.push_back(v[x[0]] == v[x[1]]);
         }
         return ans;
     }
