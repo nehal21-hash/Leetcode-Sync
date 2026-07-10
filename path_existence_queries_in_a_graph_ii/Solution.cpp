8a4862d27a2e529bbf0439ceb649163d1cc0aa7e
Md Nehal <nehalmd2955@gmail.com>
2026-07-10 22:25:32 +0530

    Automated Sync: Solved Path Existence Queries In A Graph Ii

---
+++ path_existence_queries_in_a_graph_ii/Solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int > pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<int>v(n);
+        vector<pair<int,int>>newNums(n);
+        for(int i=0;i<n;++i){
+            newNums[i] = {nums[i],i};
+        }
+        sort(newNums.begin(),newNums.end());
+        int i=0,j=0;
+        int cnt = 0;
+        while(j<n){
+            if(i == j){
+                cnt++;
+            }
+            if(abs(newNums[i].first-newNums[j].first)>maxDiff){
+                i++;
+                continue;
+            }
+            v[newNums[j].second] = cnt;
+            if(i != j){
+                i++;
+            }
+            j++;
+        }
+        vector<int>ans;
+        for(auto &x:newNums){
+            cout<<x.first<<" "<<x.second<<endl;
+        }
+        for(auto &x:queries){
+            int dif = abs(nums[x[0]]-nums[x[1]]);
+            cout<<v[x[0]]<<" "<<v[x[1]]<<endl;
+            if(v[x[0]] == v[x[1]]){
+                ans.push_back((dif+maxDiff-1)/maxDiff);
+            }else{
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
